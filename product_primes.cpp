fb807cf2d410f9255e689087b6aceb44f4fe8f4a
Math Nerd <mathnerd@anothermathblog.com>
2022-07-21 13:17:53 +0000

    Updated to take the final bound into account, as well as an optimization that lowers the search space for potential counterexamples. https://math.stackexchange.com/a/4497175/114928

--- product_primes.cpp
+++ product_primes.cpp
@@ -1,11 +1,14 @@
 #include <algorithm>
+#include <chrono>
 #include <cstdint>
 #include <fstream>
 #include <numeric>
 #include <sstream>
+#include <thread>
 #include <vector>
 
 #include <NTL/ZZ.h>
+#include <NTL/RR.h>
 #include <omp.h>
 
 
@@ -40,10 +43,14 @@ std::vector<int64_t> primes_between( std::int64_t first,
 
 int main(int argc, char **argv)
 {
-    int upper_bound = 1000000;
-    constexpr int MAX_BOUND = 10544111;
-    // Need only check up to a this limit for all possible counterexamples.
-    // https://math.stackexchange.com/a/4496643/114928
+    using namespace std::chrono;
+    auto start_time{ high_resolution_clock::now() };
+    std::ofstream out_file{ "results.txt" };
+
+    int upper_bound = 328;
+    constexpr int MAX_BOUND = 328;
+    // Need only check up to this limit for all possible counterexamples.
+    // https://math.stackexchange.com/a/4497175/114928
 
     // Get upper bound.
     if( argc > 1 )
@@ -56,12 +63,13 @@ int main(int argc, char **argv)
         }
         catch(...)
         {
-            // Stick with defaul value if something bad happens.
-            upper_bound = 1000000;
+            // Stick with default value if something bad happens.
+            upper_bound = 328;
         }
     }
 
     build_primes( "primes.txt", upper_bound );
+
 
     std::vector<Counterexample> counterexamples; // List of counterexamples.
     std::vector<Exponential> exponentials;       // List of exponentials.
@@ -86,28 +94,50 @@ int main(int argc, char **argv)
                 exponentials[i] = { NTL::ZZ{ 2 }, 1 };
             }
         }
+        
+        constexpr auto bound{ 65 };
+        // primes[65] holds 313, the largest prime needed to be considered.
 
+        // The Proposition at
+        // https://math.stackexchange.com/questions/4495973/the-product-of-primes-between-n-and-2n-compared-to-2n
+        // shows that one only needs to check numbers of the form (p_{n} - 1)/2.
+        // For any counterexample, there is one at least as big in that form.
         #pragma omp for schedule(dynamic)
-        for( auto n{ 1 }; n <= upper_bound; ++n )
+        for( auto n{ 1 }; n < bound; ++n )
         {
-            // Get primes between n and 2n.
-            auto prime_list{ primes_between( n, 2 * n ) };
+            auto N{ ( primes[n] - 1 ) / 2 };
+
+            if( n % 1000 == 0 )
+            {
+                auto current_time{ high_resolution_clock::now() };
+
+                auto current_duration{ duration_cast<microseconds>( current_time - start_time ) / 1000.0 };
+
+                std::cout << "Thread " << id << " has reached " << N << " (n = " << n << ") after "
+                          << current_duration.count() / 1000.0 << " seconds.\n";
+
+                out_file  << "Thread " << id << " has reached " << N << " (n = " << n << ") after "
+                          << current_duration.count() / 1000.0 << " seconds.\n";
+            }
+
+            // Get primes between N and 2N.
+            auto prime_list{ primes_between(N, 2 * N) };
 
             // Calculate 2^n
-            exponentials[id].value <<= n - exponentials[id].exponent;
-            exponentials[id].exponent = n;
-
+            exponentials[id].value <<= N - exponentials[id].exponent;
+            exponentials[id].exponent = N;
+            
             // Calculate product of primes from list.
             auto product{ std::accumulate( std::begin( prime_list ),
                                            std::end( prime_list ),
                                            NTL::ZZ{ 1 },
                                            []( NTL::ZZ a, std::int64_t b )
                                            { return a * b; } ) };
-
+            
             // Collect counterexamples.
             if( product < exponentials[id].value )
             {
-                buffers[id].push_back({ n, product, exponentials[id].value });
+                buffers[id].push_back({ N, product, exponentials[id].value });
             }
         }
 
@@ -120,6 +150,8 @@ int main(int argc, char **argv)
         }
     }
     // End of OpenMP block.
+
+    std::cout << "\n\n\n";
 
     std::sort( std::begin( counterexamples ), std::end( counterexamples ),
                []( Counterexample const &a, Counterexample const &b )
@@ -134,11 +166,22 @@ int main(int argc, char **argv)
         std::cout << "Product of primes between n and 2n = " << counterexample.product << "\n";
         std::cout << "2^n = " << counterexample.exp;
 
+        out_file  << "Counterexample at n = " << counterexample.n << "\n";
+        out_file  << "Product of primes between n and 2n = " << counterexample.product << "\n";
+        out_file  << "2^n = " << counterexample.exp;
+
         if( counterexample.n != last->n )
         {
             std::cout << "\n\n";
+            out_file  << "\n\n";
         }
     }
+
+    auto final_time{ high_resolution_clock::now() };
+    auto final_duration{ duration_cast<microseconds>( final_time - start_time ) / 1000.0 };
+
+    std::cout << "Final case checked after " << final_duration.count() / 1000.0 << " seconds.\n";
+    out_file  << "Final case checked after " << final_duration.count() / 1000.0 << " seconds.\n";
 
     return EXIT_SUCCESS;
 }
